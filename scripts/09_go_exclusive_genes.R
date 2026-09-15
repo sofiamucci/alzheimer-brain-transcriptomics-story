e6ffeffe4f6deb3ba7b7835e07c51fffd5f03f60
Sofia Mucci <152992001+sofiamucci@users.noreply.github.com>
2026-09-14 21:01:18 -0300

    Add GO enrichment for all three exclusive gene sets (mitochondrial, myelin, kynurenine/NLRP3 themes)

--- scripts/09_go_exclusive_genes.R
+++ scripts/09_go_exclusive_genes.R
@@ -2,6 +2,7 @@ library(clusterProfiler)
 library(org.Hs.eg.db)
 library(dplyr)
 
+# == AD versus Old exclusive genes ========
 # Load exclusive AD vs Old gene set
 exclusive_ad_old_df <- readRDS("data/genes_exclusive_ad_vs_old.rds")
 
@@ -24,3 +25,40 @@ dotplot(go_exclusive_ad_old, showCategory = 15) +
   ggtitle("GO enrichment: genes exclusive to AD vs Old")
 
 
+# == OLd versus Young exclusive genes ========
+# Load exclusive Old vs Young gene set
+exclusive_old_young_df <- readRDS("data/genes_exclusive_old_vs_young.rds")
+
+# Strip version suffix
+gene_ids_clean_oy <- sub("\\..*", "", exclusive_old_young_df$gene_id)
+
+# GO over-representation analysis
+go_exclusive_old_young <- enrichGO(gene = gene_ids_clean_oy,
+                                   OrgDb = org.Hs.eg.db,
+                                   keyType = "ENSEMBL",
+                                   ont = "ALL",
+                                   pAdjustMethod = "BH",
+                                   pvalueCutoff = 0.1,
+                                   readable = TRUE)
+
+as.data.frame(go_exclusive_old_young)
+
+
+# == AD versus Young exclusive genes ========
+
+# Load exclusive Old vs Young gene set
+exclusive_ad_young_df <- readRDS("data/genes_exclusive_ad_vs_young.rds")
+
+# Strip version suffix
+gene_ids_clean_ay <- sub("\\..*", "", exclusive_ad_young_df$gene_id)
+
+# GO over-representation analysis
+go_exclusive_ad_young <- enrichGO(gene = gene_ids_clean_ay,
+                                  OrgDb = org.Hs.eg.db,
+                                  keyType = "ENSEMBL",
+                                  ont = "ALL",
+                                  pAdjustMethod = "BH",
+                                  pvalueCutoff = 0.1,
+                                  readable = TRUE)
+
+as.data.frame(go_exclusive_ad_young)
